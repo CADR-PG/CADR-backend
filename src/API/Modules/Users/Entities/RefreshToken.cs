f3448081b788da9269c14e582b2c89bcf6e93802
mickami742 <mickami742@gmail.com>
2025-05-04 12:49:06 +0200

    dotnet format

--- src/API/Modules/Users/Entities/RefreshToken.cs
+++ src/API/Modules/Users/Entities/RefreshToken.cs
@@ -4,9 +4,9 @@ namespace API.Modules.Users.Entities;
 
 internal class RefreshToken
 {
-    public required Guid Id { get; init; }
-    public required string HashedCode { get; set; }
-    public required DateTime CreatedAt { get; set; }
-    public required DateTime ExpiresAt{ get; set; }
-    public required UserId UserId { get; set; }
+	public required Guid Id { get; init; }
+	public required string HashedCode { get; set; }
+	public required DateTime CreatedAt { get; set; }
+	public required DateTime ExpiresAt { get; set; }
+	public required UserId UserId { get; set; }
 }
