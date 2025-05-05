4598b584ab42ca1bdb824e6ea0f17373eef3ebe5
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2025-05-05 20:01:50 +0200

    key vault & azure monitor

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -3,42 +3,48 @@ using API.Modules;
 using API.Modules.Users;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+
+if (builder.Environment.IsProduction())
+{
+	builder.Configuration.AddAzureKeyVault(
+		new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+		new DefaultAzureCredential());
+
+	builder.Services.AddOpenTelemetry().UseAzureMonitor();
+}
 
 builder.Services.AddHttpContextAccessor();
 var connectionString = builder.Configuration.GetConnectionString("Database")
-		?? throw new InvalidOperationException("Connection string" + "'DefaultConnection' not found.");
+		?? throw new InvalidOperationException("Connection string" + "'Database' not found.");
 builder.Services.AddDbContext<CADRDbContext>(options =>
 	options.UseNpgsql(connectionString));
 builder.Services.AddScoped<CADRDbContext>();
 
-builder.Services.AddAuthentication().AddJwtCookie(configuration);
+builder.Services.AddAuthentication().AddJwtCookie(builder.Configuration);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddModules(configuration);
+builder.Services.AddModules(builder.Configuration);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.MapOpenApi();
-	app.MapScalarApiReference(options => options
-		.WithTitle("CADR API")
-		.WithTheme(ScalarTheme.Default)
-		.WithDarkMode(true)
-		.WithDefaultHttpClient(ScalarTarget.JavaScript, ScalarClient.Axios)
-		.WithSidebar(true)
-		.WithDownloadButton(true)
-		.WithDarkModeToggle(true)
-		.WithDotNetFlag(true)
-		.WithTestRequestButton(true)
-		.WithModels(false)
-		.WithDefaultOpenAllTags(false));
-}
+app.MapOpenApi();
+app.MapScalarApiReference(options => options
+	.WithTitle("CADR API")
+	.WithTheme(ScalarTheme.Default)
+	.WithDarkMode(true)
+	.WithDefaultHttpClient(ScalarTarget.JavaScript, ScalarClient.Axios)
+	.WithSidebar(true)
+	.WithDownloadButton(true)
+	.WithDarkModeToggle(true)
+	.WithDotNetFlag(true)
+	.WithTestRequestButton(true)
+	.WithModels(false)
+	.WithDefaultOpenAllTags(false));
 
 app.UseHttpsRedirection();
 
