109a09985c3fbcc71b81f654f27242a95258ebf0
mickami742 <mickami742@gmail.com>
2025-06-07 17:18:51 +0200

    modyfing lastupdate date after editing project

--- src/Users/Users.Core/Features/ModifyProject.cs
+++ src/Users/Users.Core/Features/ModifyProject.cs
@@ -33,8 +33,10 @@ internal sealed class ModifyProjectHandler(
 		var project = await dbContext.Projects.Where(p => p.Id == id).FirstOrDefaultAsync(cancellationToken);
 		if (project == null)
 			return Results.NotFound();
+
 		project.Name = name;
 		project.Description = description;
+		project.LastUpdate = DateTime.UtcNow;
 
 		await dbContext.SaveChangesAsync(cancellationToken);
 
