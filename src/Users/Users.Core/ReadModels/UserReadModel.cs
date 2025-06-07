e5cd4bd155e0f0b1bfe8e3f8051931e27943d081
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2025-06-07 22:23:46 +0200

    adjust readmodel

--- src/Users/Users.Core/ReadModels/UserReadModel.cs
+++ src/Users/Users.Core/ReadModels/UserReadModel.cs
@@ -2,7 +2,7 @@ using Users.Core.Entities;
 
 namespace Users.Core.ReadModels;
 
-internal record UserReadModel(Guid Id, string Email, string FirstName, string LastName, DateTime LastLoggedInAt)
+internal record UserReadModel(Guid Id, string Email, bool IsEmailConfirmed, string FirstName, string LastName, DateTime LastLoggedInAt)
 {
-	public static UserReadModel From(User user) => new(user.Id, user.Email, user.FirstName, user.LastName, user.LastLoggedInAt);
+	public static UserReadModel From(User user) => new(user.Id, user.Email, user.EmailConfirmation.IsConfirmed, user.FirstName, user.LastName, user.LastLoggedInAt);
 };
