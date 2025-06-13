d04f2555ed6eb4cc7c3cc375d455be0a9a458544
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2025-06-13 11:46:24 +0200

    feat: cors local (#27)

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
 	options.AddPolicy(name: CorsPolicyName,
 		policy =>
 		{
-			policy.WithOrigins("https://localhost:5173/", "https://cadr.studio/")
+			policy.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://cadr.studio")
 				.AllowAnyHeader()
 				.AllowAnyMethod()
 				.AllowCredentials();
