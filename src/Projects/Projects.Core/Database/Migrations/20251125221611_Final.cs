f84e495d945c7f633968cb6f8bb2410066a9cd9f
mickami742 <mickami742@gmail.com>
2025-11-25 23:17:04 +0100

    migration which is adding FileSize property

---
+++ src/Projects/Projects.Core/Database/Migrations/20251125221611_Final.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projects.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "FileSize",
+                schema: "Projects",
+                table: "Assets",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileSize",
+                schema: "Projects",
+                table: "Assets");
+        }
+    }
+}
