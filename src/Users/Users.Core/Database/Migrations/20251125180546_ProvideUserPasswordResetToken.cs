d1148db235fc266471e609dd4a7079b1020ce95e
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2025-11-25 19:58:39 +0100

    migrations

---
+++ src/Users/Users.Core/Database/Migrations/20251125180546_ProvideUserPasswordResetToken.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProvideUserPasswordResetToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailConfirmation_Code",
+                schema: "Users",
+                table: "Users",
+                type: "character varying(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailConfirmation_Email",
+                schema: "Users",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetExpiresAt",
+                schema: "Users",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PasswordResetToken",
+                schema: "Users",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmation_Email",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetExpiresAt",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmailConfirmation_Code",
+                schema: "Users",
+                table: "Users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+        }
+    }
+}
