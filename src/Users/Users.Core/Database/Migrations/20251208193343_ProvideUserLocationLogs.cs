46f70a9e3181476bcc712c46714c892138b2cb0e
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2025-12-08 20:39:10 +0100

    migrations

---
+++ src/Users/Users.Core/Database/Migrations/20251208193343_ProvideUserLocationLogs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProvideUserLocationLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLocationLogs",
+                schema: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthenticationType = table.Column<int>(type: "integer", nullable: false),
+                    OccuredAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IpAddress = table.Column<string>(type: "character varying(39)", maxLength: 39, nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    Country = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    City = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLocationLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserLocationLogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Users",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLocationLogs_UserId",
+                schema: "Users",
+                table: "UserLocationLogs",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserLocationLogs",
+                schema: "Users");
+        }
+    }
+}
